cf7ec5d340c677cef0547a5127caaacdb990ec63
Eubert Moracha <147544426+eubertmoracha@users.noreply.github.com>
2025-07-11 19:04:49 +0300

    Add files via upload
    Write a script that prints a message depending of the number of arguments passed:
    
    If no arguments are passed to the script, print “No argument”
    If only one argument is passed to the script, print “Argument found”
    Otherwise, print “Arguments found”
    You must use console.log(...) to print all output

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+const args = process.argv.slice(2);
+
+// Check the number of arguments in our new 'args' array.
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
