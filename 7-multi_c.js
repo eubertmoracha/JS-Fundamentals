d5a71586eb80877c15e3fc2c73edd3db1c493341
Eubert Moracha <147544426+eubertmoracha@users.noreply.github.com>
2025-07-12 09:13:44 +0300

    Add files via upload
    Write a script that prints x times “C is fun”
    
    Where x is the first argument of the script
    If the first argument can’t be converted to an integer, print “Missing number of occurrences”
    You must use console.log(...) to print all output
    You are not allowed to use var
    You can use only two console.log
    You must use a loop (while, for, etc.)

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,18 +1,13 @@
-// First, create an array of strings. An array is just a list of items.
-// We use `const` because this list of sentences will not change.
-const myLines = [
-  'C is fun',
-  'Python is cool',
-  'JavaScript is amazing'
-];
+// 7-multi_c.js
+// This script prints "C is fun" a specified number of times.
+// The number of times is provided as a command line argument.
 
-// Now, we use a `for...of` loop to go through each item in our array.
-// For each iteration, the next item from `myLines` is put into the `line` variable.
-// 1st time through: line = 'C is fun'
-// 2nd time through: line = 'Python is cool'
-// 3rd time through: line = 'JavaScript is amazing'
-for (const line of myLines) {
-  // This is our one and only console.log statement.
-  // It runs once for each item in the array, printing the current `line`.
-  console.log(line);
+const count = parseInt(process.argv[2], 10);
+
+if (isNaN(count)) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < count; i++) {
+    console.log("C is fun");
+  }
 }
